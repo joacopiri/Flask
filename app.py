03bbedf2464cee16edbf49c77fd1d6fd9c970a32
Joaquin Pirillo <joaquinpirillo5102007>
2025-04-16 11:54:49 -0300

    avance un poco con el codigo 'app.py'

--- app.py
+++ app.py
@@ -1,7 +1,80 @@
-from flask import Flask
+from flask import Flask, url_for
+import sqlite3
+
+app = Flask(__name__)
+db = None
+
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
+#--------------------CONEXION A BASE DE DATOS--------------------
+def abrirConexion():
+    global db
+    db = sqlite3.connect('instance/datos.sqlite')
+    db.row_factory = sqlite3.Row
+    return db
+
+def cerrrarConexion():
+    global db
+    if db is not None:
+        db.close()
+        db = None
+
+
+@app.route("/usuario")
+def obtenerGente():
+    global db
+    conexion = abrirConexion()
+    cursor = conexion.cursor()
+    cursor.execute('SELECT * FROM usuarios')
+    resultado = cursor.fetchall()
+    cerrrarConexion()
+    fila = [dict(row) for row in resultado]
+    return str(fila)
+#----------------------------------------------------------------
+
+
+#----------------------------------------------------------------
+@app.route("/jiji")
+def jijiji():
+    return "<h2>MUEJEJEJE</h2>"
+
+@app.route("/chau")
+def jejeje():
+    return "<h2>muejejeje</h2>"
+
+@app.route("/sumar/<int:n1>/<int:n2>")
+def suma(n1, n2):
+    suma = n1+n2
+    return F"<h2>{n1} más {n2} es igual a {suma}</h2>"
+
+@app.route("/tirar-dado/<int:caras>")
+def dado (caras):
+    from random import randint
+    n = randint(1,caras)
+    return f"<p>Tire un dado de {caras} caras, salio {n}</p>"
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main():
+    url_hola = url_for("jijiji")
+    url_dado = url_for("dado",caras=6)
+    url_logo = url_for("static",filename="miau.jpeg")
+
+    return f"""
+    <a href= "{url_hola}">Hola</a>
+    <br>
+    <a href= "{url_for("jejeje")}">Chau</a>
+    <br>
+    <a href= "{url_logo}">Logo</a>
+    <br>
+    <a href= "{url_dado}">Tirar_dado</a>
+    """
+#----------------------------------------------------------------
+
+
+
+
+
+
